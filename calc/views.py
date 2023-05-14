4b80ead1b0f5dd8017871db887560b41cc921864
Danusshkumar <mdanusshkumar@gmail.com>
2023-05-14 21:03:31 +0530

    Printed hello world

---
+++ calc/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request,'add.html')
+
+def add(request):
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    sum = val1 + val2
+    return render(request, 'result.html',{"result" : sum})
